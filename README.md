aefded27c1ee29b1239285de1d8738b8b995a822
barsich <barsicchan@gmail.com>
2021-05-09 15:11:53 +0500

    destructuring hw added

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# destructuring
+# destructuring [![Build status](https://ci.appveyor.com/api/projects/status/bl94jkih8ujstxyr/branch/main?svg=true)](https://ci.appveyor.com/project/barsich/destructuring/branch/main)
---
+++ src/js/__tests__/app.test.js
@@ -0,0 +1,19 @@
+import destruct, { character } from '../app';
+
+test('destruct', () => {
+  const expected = [
+    {
+      id: 8,
+      name: 'Двойной выстрел',
+      icon: 'http://...',
+      description: 'Двойной выстрел наносит двойной урон',
+    },
+    {
+      id: 9,
+      name: 'Нокаутирующий удар',
+      icon: 'http://...',
+      description: 'Описание недоступно',
+    },
+  ];
+  expect(expected).toEqual(destruct(character));
+});
